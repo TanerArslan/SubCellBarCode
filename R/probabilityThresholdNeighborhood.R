430ff98a915bb346bb0c342a0d2009c791cb5348
TanerArslan <tanerarslan.gen@gmail.com>
2019-03-27 12:10:50 +0100

    redundacy deleted

--- R/probabilityThresholdNeighborhood.R
+++ R/probabilityThresholdNeighborhood.R
@@ -37,28 +37,6 @@ computeThresholdNeighborhood <- function(test.repA, test.repB){
                                                     column = "svm.pred")
 
     # concatanate the probabilities for corresponding neighborhood
-    #sumProbability <- function(df){
-
-        #t.secretory.df <- data.frame(df[, colnames(df)[3:6]])
-        #t.secretory.df$Secretory <- apply(t.secretory.df, 1, sum)
-        #t.nuclear.df <- data.frame(df[, colnames(df)[7:10]])
-        #t.nuclear.df$Nuclear <- apply(t.nuclear.df, 1, sum)
-        #t.cytosol.df <- data.frame(df[, colnames(df)[11:15]])
-        #t.cytosol.df$Cytosol <- apply(t.cytosol.df, 1, sum)
-        #t.Mitochondria.df <- data.frame(df[, colnames(df)[16:17]])
-        #t.Mitochondria.df$Mitochondria <- apply(t.Mitochondria.df, 1, sum)
-
-        #summed.df <- data.frame(Proteins = rownames(df),
-        #                        df[,colnames(df)[seq_len(2)]],
-        #                        Secretory = t.secretory.df$Secretory,
-        #                        Nuclear = t.nuclear.df$Nuclear,
-        #                        Cytosol = t.cytosol.df$Cytosol,
-        #                        Mitochondria = t.Mitochondria.df$Mitochondria)
-        #temp neighborhood df
-        #t.n.df <- summed.df[,4:7]
-        #summed.df$svm.pred <- colnames(t.n.df)[apply(t.n.df, 1, which.max)]
-        #return(summed.df)
-    #}
 
     sum.repA <- SubCellBarCode::sumProbability(neighborhood.repA)
     sum.repB <- SubCellBarCode::sumProbability(neighborhood.repB)
